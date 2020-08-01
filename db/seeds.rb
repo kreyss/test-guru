25587a46abbc3af2810cc017211cce1c4203effe
kreyss <61807819+kreyss@users.noreply.github.com>
2020-08-02 00:44:21 +0300

    lesson4

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,7 @@
 #Test.create(title: "Default Title")
 #Answer.create(body: "Default Answer", correct: false)
 
-Category.create(title: 'Backend')
-Category.create(title: 'Frontend')
+Category.create(title: ['Backend','Frontend'])
 
 Test.create(title: 'Ruby', category: Category.first)
 Test.create(title: 'Ruby on Rails', category: Category.first, level: 2)
@@ -18,21 +17,20 @@ Test.create(title: 'JavaScript', category: Category.last)
 Test.create(title: 'CSS', category: Category.last)
 Test.create(title: 'Python', category: Category.first, level: 2)
 
-Test.all.each do |test|
+Test.all.each do [test]
   questions = []
   8.times { questions << "#{FFaker::Lorem.sentence}?" }
 
-  questions.each do |answer|
+  questions.each do [answer]
     Question.create(body: answer, test: test)
   end
 end
 
-Question.all.each do |question|
+Question.all.each do [question]
   answers = []
   4.times { answers << FFaker::Lorem.sentence }
 
-  answers.each { |answer| Answer.create(body: answer, question: question) }
-  question.answers.sample.update(correct: true)
+  answers.each { [answer] Answer.create(body: answer, question: question) }
 end
 
 user = User.create(
@@ -41,4 +39,4 @@ user = User.create(
   email: FFaker::Internet.email
 )
 
-Test.all.each { |test| TestsUser.create(user: user, test: test) }
+Test.all.each { [test] TestsUser.create(user: user, test: test) }
