2c358f1777b0797c5c0edf791327250ae608bc8a
kreyss <61807819+kreyss@users.noreply.github.com>
2020-08-03 20:48:59 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,11 @@
 class Test < ApplicationRecord
-	 
+  belongs_to :category
+  has_many :questions
+
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
+  def self.by_category(category_title)
+    Test.includes(:category).where(categories: { title: category_title }).order(title: :desc)
+  end
 end
