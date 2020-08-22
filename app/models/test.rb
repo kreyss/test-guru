92976c09b4b150caef6bcff1760b389d354c3c34
kreyss <61807819+kreyss@users.noreply.github.com>
2020-08-23 00:42:06 +0300

    lesson5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,11 @@
 class Test < ApplicationRecord
-  belongs_to :author, class_name: 'User'
   belongs_to :category
-
   has_many :questions
+
   has_many :tests_users
   has_many :users, through: :tests_users
 
   def self.by_category(category_title)
-    Test.includes(:category).where(categories: { title: category_title }).order(title: :desc)
+    Test.includes(:category).where(categories: { title: category_title }).order(title: :desc)ы
   end
 end
--- db/migrate/20200806101355_add_author_to_tests.rb
+++ db/migrate/20200806101355_add_author_to_tests.rb
@@ -1,5 +1,5 @@
 class AddAuthorToTests < ActiveRecord::Migration[6.0]
   def change
-  	add_reference :tests, :authur
+  	add_reference :tests, :author
   end
 end
