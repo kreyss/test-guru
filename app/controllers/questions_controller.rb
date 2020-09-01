1d48bee3f47a9f911d40f89a34fe3d6d2a500f34
kreyss <61807819+kreyss@users.noreply.github.com>
2020-09-01 21:50:53 +0300

    lesson7

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,50 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: [:index, :new, :create]
+  before_action :find_question, only: [:show, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to test_questions_path(@question.test)
+  end
+
+  private
+
+  def find_test
+    @test ||= Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  def rescue_with_not_found
+    render plain: 'Question Not Found', status: 404
+  end
+
+end
