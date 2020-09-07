481f3320782dd337c0cce88e4fb7280c35994b99
kreyss <61807819+kreyss@users.noreply.github.com>
2020-09-07 22:50:04 +0300

    lesson9

--- app/models/users.rb
+++ app/models/users.rb
@@ -2,9 +2,13 @@ class Users < ApplicationRecord
   enum role: %i[admin regular]
 
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   validates :user_name, :email, presence: true
+
+  def test_passages(test)
+  	test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 
 end
