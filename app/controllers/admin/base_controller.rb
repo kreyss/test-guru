5316ac1869fbffeb26d1b4be7356a3b87e1835b1
kreyss <61807819+kreyss@users.noreply.github.com>
2020-09-20 21:46:50 +0300

    lesson12

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,10 @@ class Admin::BaseController < ApplicationController
 
   before_action :admin:required!
 
+    def default_url_options
+      { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+    end
+
   private
 
   def admin_required!
@@ -10,4 +14,9 @@ class Admin::BaseController < ApplicationController
       redirect_to root_path, alert: "You are not authorized to view this page"
     end
   end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
+
 end
--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,15 @@
 module TestPassagesHelper
+  def show_score
+    content_tag :h3, @test_passage.correct_questions, class: "result-#{result_type}"
+  end
+
+  def show_message
+    content_tag :p, "#{result_type == 'success' ? t('test_passages.result.success') : t('test_passages.result.fail')}"
+  end
+
+  private
+
+  def result_type
+    @result >= 0.85 ? 'success' : 'fail'
+  end
 end
