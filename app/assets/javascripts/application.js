0e7cb179a21f0fa3fd221d9057d14f2931dba72b
kreyss <61807819+kreyss@users.noreply.github.com>
2020-10-16 13:57:56 +0300

    lesson15

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
+
+
+document.addEventListener('turbolinks:load', function () {
+  $('#user_password_confirmation').on('keyup', function () {
+    let passwordField = document.querySelector('#user_password')
+    let messageBox    = document.querySelector('p.confirmation-message')
+
+    if (this.value == '') {
+      messageBox.innerText = ''
+    } else if (this.value == passwordField.value) {
+      messageBox.innerText = 'Passwords are equal'
+      messageBox.classList.remove('red')
+      messageBox.classList.add('green')
+    } else {
+      messageBox.innerText = 'Passwords are not equal'
+      messageBox.classList.remove('green')
+      messageBox.classList.add('red')
+    }
+  })
+})
